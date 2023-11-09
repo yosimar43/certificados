839d6bddc77fbf2928d5118c76bfe511f465f1c5
yosimar43 <72826438+yosimar43@users.noreply.github.com>
2023-11-09 15:34:23 -0500

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       });
     }
   }
-  const hanldeSubmit = e => router.push(`/certificados?nombre${"hola"}`)
+  const hanldeSubmit = () => router.push(`/certificados?nombre${"hola"}`)
 
   const documentos = [
     {
